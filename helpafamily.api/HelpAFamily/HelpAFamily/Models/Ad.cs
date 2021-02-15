44d763c4403d8440c5e3b41521e1de128aaac073
gabrielaspinu <73698622+gabrielaspinu@users.noreply.github.com>
2021-02-15 21:17:06 +0200

    Initial commit

---
+++ helpafamily.api/HelpAFamily/HelpAFamily/Models/Ad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpAFamily.Models
+{
+    [Table("Ads")]
+    public class Ad
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UserId { get; set; }
+        public string Picture { get; set; }
+        public int TypeId { get; set; }
+        public int DeliveryTypeId { get; set; }
+        public string Status { get; set; }
+
+    }
+}
---
+++ helpafamily.api/HelpAFamily/HelpAFamily/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpAFamily.Models
+{
+    [Table ("Users")]
+    public class User
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Role { get; set; }
+        [ForeignKey("UserId")]
+    }
+}
